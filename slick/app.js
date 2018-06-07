03eca2f5eb36e8653739675476d4f81a3ae3a32d
Christopher Gracia <emeria@outlook.com>
2018-06-06 22:33:06 -0400

    +added carousel slick js with pictures

---
+++ slick/app.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+  $('.slideshowWedding').slick({
+  lazyLoad: 'ondemand',
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 1500,
+  arrows: false,
+  adpativeHeight: true,
+  });
+    
+$('.slideshowStay').slick({
+  lazyLoad: 'ondemand',
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 1500,
+  arrows: false,
+  adpativeHeight: true,
+  });
+    
+$('.slideshowStory').slick({
+  lazyLoad: 'ondemand',
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 1500,
+  arrows: false,
+  adpativeHeight: true,
+  });
+    
+$('.slideshowRegistry').slick({
+  lazyLoad: 'ondemand',
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 1500,
+  arrows: false,
+  adpativeHeight: true,
+    
+  });
+});
